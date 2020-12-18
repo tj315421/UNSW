78f1dea7bf365e070c5d91222ce0443f426e45e4
double1 <71979670+tj315421@users.noreply.github.com>
2020-12-18 16:52:02 +1100

    Add files via upload

---
+++ Programming Fundamentals/Week1/Labs/bird.c
@@ -0,0 +1,23 @@
+// A simple C program that attempts to be punny
+// Written 21/09/2020
+// by WangLiao (5306312@ad.unsw.edu.au)
+// for COMP1511 Lab 01 Exercise 2
+
+/* 
+  ___
+ ('v')
+((___))
+ ^   ^
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("  ___\n");
+    printf(" ('v')\n");
+    printf("((___))\n");
+    printf(" ^   ^\n");
+                
+    return 0;
+}
---
+++ Programming Fundamentals/Week1/Labs/kangaroo.c
@@ -0,0 +1,21 @@
+
+// A simple C program that prints an ASCII kangaroo
+// Written 21/09/2020
+// by WangLiao (5306312@ad.unsw.edu.au)
+// for COMP1511 Lab 01 Exercise 3
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("\n");
+    printf("  /\\   ___\n");
+    printf(" <__\\_/    \\\n");
+    printf("    \\_  /  _\\\n");
+    printf("      \\_\\ / \\\\\n");
+    printf("        //   \\\\\n");
+    printf("      =//     \\==\n");
+    printf("\n");
+
+    return 0;
+}
