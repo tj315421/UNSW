f863e28bd0152c323873a7b3d940e29c2e95b7d3
double1 <71979670+tj315421@users.noreply.github.com>
2020-12-18 16:50:28 +1100

    Add files via upload

--- Programming Fundamentals/Week1/Labs/bad_pun.c
+++ Programming Fundamentals/Week1/Labs/bad_pun.c
@@ -1,1 +1,31 @@
+// A simple C program that attempts to be punny
+// Written 23/2/2017
+// by Angela Finlayson (angf@cse.unsw.edu.au)
+// for COMP1511 Lab 01 Exercise 1
+
+/* 
+in linux system highlight + mid button = ctrl c + v
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+void recursive_d(int array[128], int i);
+int main (void) {
+    int array[128] = {0};
+    int i = 7;
+    recursive_d(array, i);
+    printf("%d", i);
+    return 0;
+}
+
+// assume array is a valid array of integers,
+// assume i is any integer.
+void recursive_d(int array[128], int i) {
+    if (i < 0) {
+        return;
+    }
+    array[i] = i;
+    recursive_d(array, i - 1);
+}
 
